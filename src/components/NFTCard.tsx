af4a7308f9f433e47819c4f307ca8907c9df41a0
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2024-04-23 19:11:23 +0200

    Added Traditional Staking

--- src/components/NFTCard.tsx
+++ src/components/NFTCard.tsx
@@ -1,39 +1,29 @@
-import { ThirdwebNftMedia, useNFT, useContract, useContractRead, useAddress } from "@thirdweb-dev/react";
-import { STAKE_ADDRESS } from "../consts/addresses";
-import { ethers } from "ethers";
+import {
+  ThirdwebNftMedia,
+  useNFT,
+  useContract,
+  Web3Button,
+} from "@thirdweb-dev/react";
+import { DOONZ_ADDRESS, STAKE_ADDRESS } from "../consts/addresses";
 
 interface NFTCardProp {
-    tokenId: number;
-    contractAddress:string
-    selectedNFT:number;
+  tokenId: number;
 }
-const NFTCard = (props: NFTCardProp) => {
-    const address = useAddress();
-    const { contract: NftContract } = useContract(props.contractAddress, 'nft-drop')
-    const {data : nft} = useNFT(NftContract,props.tokenId)
-  const { contract:StakeContract } = useContract(STAKE_ADDRESS);
-    const {data:stakenNFTs} = props.selectedNFT ==1 ? useContractRead(StakeContract,"getStakedTokenIdsOfOwner",[address]): useContractRead(StakeContract,"getStakedFBTokenIdsOfOwner",[address])
-    const toNumberStakedNFTs = stakenNFTs ? stakenNFTs.map((item: ethers.BigNumberish) => ethers.utils.formatUnits(item,0)) : [];
-    const isStaked = toNumberStakedNFTs.includes(props.tokenId.toString());
-    console.log(stakenNFTs)
-    return (
-        <div className="text-center bg-white p-4 m-2 rounded-lg w-[200px]">
-            {nft && <h3>Doonz {nft.metadata.name}</h3>}
-            {nft?.metadata && <ThirdwebNftMedia
-            metadata={nft.metadata} />}
-            {isStaked? (
-                <div className="text-green-500 p-2 border border-black rounded-xl">Staked</div>
-            ):(
-                <div className="text-red-500 p-2 border-black rounded-xl">Not Staked</div>
-
-
-            )}
-
-        </div>
-
-    )
-
-
-}
+const NFTCard = ({ tokenId }: NFTCardProp) => {
+  const { contract } = useContract(DOONZ_ADDRESS, "nft-drop");
+  const { data: nft } = useNFT(contract, tokenId);
+  return (
+    <div className="text-center bg-white p-4 m-2 rounded-lg w-[200px]">
+      {nft && <h3>Doonz {nft.metadata.name}</h3>}
+      {nft?.metadata && <ThirdwebNftMedia metadata={nft.metadata} />}
+      <Web3Button
+        action={(contract) => contract?.call("withdraw", [[nft?.metadata.id]])}
+        contractAddress={STAKE_ADDRESS}
+      >
+        Withdraw
+      </Web3Button>
+    </div>
+  );
+};
 
 export default NFTCard;
--- src/components/pages/stake/Stake.tsx
+++ src/components/pages/stake/Stake.tsx
@@ -1,97 +1,171 @@
-import { SmartContract, Web3Button, useAddress, useContract, useContractRead, useOwnedNFTs } from "@thirdweb-dev/react";
-import { DOONZ_ADDRESS, FOOTBALL_ADDRESS, STAKE_ADDRESS } from "../../../consts/addresses";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  ThirdwebNftMedia,
+  Web3Button,
+  useAddress,
+  useContract,
+  useContractRead,
+  useOwnedNFTs,
+  useTokenBalance,
+} from "@thirdweb-dev/react";
+import {
+  DOONZ_ADDRESS,
+  STAKE_ADDRESS,
+  TOKEN_ADDRESS,
+} from "../../../consts/addresses";
 import NFTCard from "../../NFTCard";
-import { BaseContract, ethers } from "ethers";
-import SwitchableItems from "../../ui/SwitchableItems";
-import { useState } from "react";
+import { BigNumber, ethers } from "ethers";
 import BoosterButton from "../../BoosterButton";
 import { CircularProgressbar } from "react-circular-progressbar";
-import 'react-circular-progressbar/dist/styles.css';
-import '../../../assets/styles/Button.css'
+import "react-circular-progressbar/dist/styles.css";
+import "../../../assets/styles/Button.css";
 
 export default function Stake() {
   const address = useAddress();
-  const { contract } = useContract(STAKE_ADDRESS);
-  const { data:NFTContract } = useContract(DOONZ_ADDRESS, 'nft-drop');
-  const { data:NFTNftContract } = useContract(FOOTBALL_ADDRESS, 'nft-drop');
-
-  const { data: accumlatedRewards } = useContractRead(
-    contract,
-    "viewAccumulatedRewards",
-    [address]
-  );
-  const { data: boosterPercent,isLoading } = useContractRead(
+  const { contract, isLoading: isLoadingStake } = useContract(STAKE_ADDRESS);
+  const { data: NFTContract } = useContract(DOONZ_ADDRESS, "nft-drop");
+  const { data: boosterPercent, isLoading } = useContractRead(
     contract,
     "getMultiplier",
     [address]
   );
-  const actualPercentage = boosterPercent? boosterPercent.toString() : 0;
+  const { data: userStakeInfo } = useContractRead(contract, "userStakeInfo", [
+    address,
+  ]);
+  const actualPercentage = boosterPercent ? boosterPercent.toString() : 0;
   const { data: ownedNFTs } = useOwnedNFTs(NFTContract, address);
-  const { data: ownedFootballNFTs } = useOwnedNFTs(NFTNftContract, address);
 
-  const [selectedNFT, setSelectedNFT] = useState<number>(1);
-  const handleItemClick = (item: number) => {
-    setSelectedNFT(item);
-  };
+  const { contract: tokenContract } = useContract(TOKEN_ADDRESS, "token");
+
+  const { data: tokenBalance } = useTokenBalance(tokenContract, address);
+
+  async function stakeNft(id: string) {
+    if (!address) return;
+
+    const isApproved = await NFTContract?.isApproved(address, STAKE_ADDRESS);
+    if (!isApproved) {
+      await NFTContract?.setApprovalForAll(STAKE_ADDRESS, true);
+    }
+    await contract?.call("stake", [[id]]);
+  }
+
+  async function stakeAllNft(ids: any[]) {
+    if (!address) return;
+
+    const isApproved = await NFTContract?.isApproved(address, STAKE_ADDRESS);
+    if (!isApproved) {
+      await NFTContract?.setApprovalForAll(STAKE_ADDRESS, true);
+    }
+    await contract?.call("stake", [ids]);
+  }
+
+
+
+  if (isLoadingStake) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="w-full flex items-start justify-evenly h-full">
       <div className="lg:grid lg:grid-cols-2 lg: flex flex-col justify-evenly  xl:max-w-[1280px] w-full mt-[11vw] relative z-10  m-0 overflow-hidden">
-      <div className="border-white border-4 bg-opacity-65 rounded-3xl max-h-[60vh] bg-white flex flex-col items-center justify-between mt-[110px] p-5">
-      <div className="w-full flex justify-center">
-          <div className="w-fit h-auto p-2 m-2 flex flex-col justify-center items-center whitespace-nowrap bg-blue-500 rounded-md text-white">
-            <span>Accumulated Rewards:</span>
-            <span>{accumlatedRewards && parseFloat(ethers.utils.formatUnits(accumlatedRewards, 18)).toFixed(2)} $DNZ</span>
+        <div className="border-white border-4 bg-opacity-65 rounded-3xl max-h-[60vh] bg-white flex flex-col items-center justify-between mt-[110px] p-5">
+          <div className="w-full flex justify-center">
+            <div className="w-fit h-auto p-2 m-2 flex flex-col justify-center items-center whitespace-nowrap bg-blue-500 rounded-md text-white">
+              <span>Accumulated Rewards:</span>
+              <span>
+                {!userStakeInfo
+                  ? "Loading..."
+                  : ethers.utils.formatUnits(userStakeInfo[1], 18)}{" "}
+                {tokenBalance?.symbol}
+              </span>
+            </div>
+          </div>
+          <BoosterButton />
+          {!isLoading && (
+            <CircularProgressbar
+              className="m-10"
+              value={actualPercentage}
+              maxValue={100}
+              text={`+${actualPercentage}%`}
+            />
+          )}
+
+          <Web3Button
+            className="button"
+            action={(contract) => contract.call("claimRewards")}
+            contractAddress={STAKE_ADDRESS}
+          >
+            Claim Rewards
+          </Web3Button>
+        </div>
+        <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center">
+            <span className="text-2xl text-white">Staked NFTs</span>
+            <div className="p-4 m-5 border-white border-4 bg-opacity-65 rounded-3xl overflow-auto max-h-[60vh] bg-white flex flex-wrap justify-center md:grid md:grid-cols-2">
+              {userStakeInfo && userStakeInfo[0].length === 0 && (
+                <div>You do not have any NFT Staked</div>
+              )}
+              {userStakeInfo &&
+                userStakeInfo[0]?.map((stakedToken: BigNumber) => (
+                  <NFTCard
+                    tokenId={stakedToken.toNumber()}
+                    key={stakedToken.toString()}
+                  />
+                ))}
+            </div>
+            {userStakeInfo && userStakeInfo[0]?.length > 0 && (
+              <Web3Button
+                className="button"
+                contractAddress={STAKE_ADDRESS}
+                action={() => {
+                  const tokenIds = ownedNFTs?.map((nft) => nft.metadata.id);
+                  if (!tokenIds) return;
+                  return contract?.call("withdraw", [tokenIds]);
+                }}
+              >
+                Withdrw all
+              </Web3Button>
+            )}
+
+            <div className="flex flex-col items-center">
+              <span className="text-2xl text-white">Unstaked NFTs</span>
+
+              <div className="p-4 m-5 border-white border-4 bg-opacity-65 rounded-3xl overflow-auto max-h-[60vh] bg-white flex flex-wrap justify-center md:grid md:grid-cols-2">
+                {ownedNFTs && ownedNFTs.length === 0 && (
+                  <div>You do not own any unstaked NFTs</div>
+                )}
+                {ownedNFTs?.map((nft) => (
+                  <div
+                    className="text-center bg-white p-4 m-2 rounded-lg w-[200px]"
+                    key={nft.metadata.id.toString()}
+                  >
+                    <h3>{nft.metadata.name}</h3>
+                    <ThirdwebNftMedia metadata={nft.metadata} />
+                    <Web3Button
+                      contractAddress={STAKE_ADDRESS}
+                      action={() => stakeNft(nft.metadata.id)}
+                    >
+                      Stake
+                    </Web3Button>
+                  </div>
+                ))}
+              </div>
+              {ownedNFTs && ownedNFTs?.length > 0 && (
+                <Web3Button
+                  className="button"
+                  contractAddress={STAKE_ADDRESS}
+                  action={() => {
+                    const tokenIds = ownedNFTs?.map((nft) => nft.metadata.id);
+                    if (!tokenIds) return;
+                    return stakeAllNft(tokenIds);
+                  }}
+                >
+                  Stake all
+                </Web3Button>
+              )}
+            </div>
           </div>
         </div>
-        <BoosterButton/>
-        {!isLoading && <CircularProgressbar className="m-10" value={actualPercentage} maxValue={100} text={`+${actualPercentage}%`}/>}
-        <div className="flex gap-5">
-        <Web3Button className="button" contractAddress={STAKE_ADDRESS} action={ async function (contract: SmartContract<BaseContract>) {
-                if (!address) return;
-                  selectedNFT == 1? await contract?.call("stake"): await contract?.call("fbstake");
-          } }>Stake All {selectedNFT==1?"Doonz":"Football Doonz"}</Web3Button>
-                  <Web3Button className="button" contractAddress={STAKE_ADDRESS} action={ async function (contract: SmartContract<BaseContract>) {
-                if (!address) return;
-                  await contract?.call("claimRewards");
-          } }>Claim Rewards </Web3Button>
-          </div>
-      </div>
-      <div className="flex flex-col items-center">
-      <SwitchableItems selectedItem={selectedNFT} onItemClick={handleItemClick} />
-        <span className="text-2xl text-white">Owned NFTs</span>
-        <div className="p-4 m-5 border-white border-4 bg-opacity-65 rounded-3xl overflow-auto max-h-[60vh] bg-white flex flex-wrap justify-center md:grid md:grid-cols-2"> 
-        {selectedNFT === 1 && (!ownedNFTs || ownedNFTs.length === 0) && (
-  "You don't own any Doonz!"
-)}
-
-{selectedNFT === 1 && ownedNFTs && ownedNFTs.length > 0 && (
-  ownedNFTs.map((nft, i) => (
-    <NFTCard
-      key={i}
-      tokenId={Number(nft.metadata.id)}
-      contractAddress={DOONZ_ADDRESS}
-      selectedNFT={selectedNFT}
-    />
-  ))
-)}
-
-{selectedNFT === 2 && (!ownedFootballNFTs || ownedFootballNFTs.length === 0) && (
-  "You don't own any football NFTs!"
-)}
-
-{selectedNFT === 2 && ownedFootballNFTs && ownedFootballNFTs.length > 0 && (
-  ownedFootballNFTs.map((nft, i) => (
-    <NFTCard
-      key={i}
-      tokenId={Number(nft.metadata.id)}
-      contractAddress={FOOTBALL_ADDRESS}
-      selectedNFT={selectedNFT}
-    />
-  ))
-)}
-        </div>
-      </div>
-
       </div>
     </div>
   );
