bf83845ad30961de402fb51e0767d2de499a66a5
Mokanji <103073614+MohamedWael3011@users.noreply.github.com>
2024-04-23 19:25:53 +0200

    Rollback

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,9 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  build: {
-    target: 'esnext', // you can also use 'es2020' here
-  },
   define: {
     "process.env": {
       VITE_TEMPLATE_CLIENT_ID: JSON.stringify(process.env.VITE_TEMPLATE_CLIENT_ID)
-    },
-
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: 'esnext', // you can also use 'es2020' here
     },
   },
 });
